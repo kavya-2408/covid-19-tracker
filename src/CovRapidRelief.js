686943000682e8a9d2e3d96b748f217cda7a1a55
Kavya <bajajkavya48@gmail.com>
2021-06-03 22:38:28 +0530

    changes

---
+++ src/CovRapidRelief.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from "./logo.png"
+
+export default function CovRapidRelief() {
+    return (
+        <div>
+            <h2>If you need any leads in finding medical aid, please visit
+            <a href="https://cov-rapid-relief.vercel.app/remdesivir.html" target="blank"><img className="logo" src={logo} alt="kaya"/></a>
+            </h2>
+        </div>
+    )
+}
